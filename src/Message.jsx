31c6e12b7eaef503dc3a9715ca152abaeb2f8c90
OLEKSANDR VYSHNYVETSKYI <vishniveckijj1994@gmail.com>
2025-02-14 17:25:44 +0200

    bild the app

--- src/Message.jsx
+++
@@ -1,9 +0,0 @@
-function Message({ text }) {
-    return (
-        <h1 className="message">
-            {text === "" || !text ? "Это не возможно !" : text}
-        </h1>
-    );
-}
-
-export default Message;
